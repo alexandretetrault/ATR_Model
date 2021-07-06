1973259a8edc9487c932c4005550705064d11676
alexandretetrault <fieldofsheep@gmail.com>
2021-07-06 10:26:19 -0400

    July6

--- Next_Gen_Model.R
+++ Next_Gen_Model.R
@@ -9,7 +9,7 @@ library(Hmisc)
 #### Input Code for Compounds ####
 
 #Load ATR Spectral Data File Names
-file_names <- list.files(pattern = "*.asp")
+file_names <- list.files(path = "Spectra", pattern = "*.asp")
 
 #Create String of Compounds
 Compounds <- print(paste(gsub("\\(.*", "", file_names)))
@@ -40,7 +40,7 @@ spectra <- sapply(file_names,
                   USE.NAMES = TRUE,
                   simplify = FALSE,
                   function(x){
-  cbind(spectra[[x]], read.csv(x,
+  cbind(spectra[[x]], read.csv(paste0("./Spectra/", x),
                                skip = 6,
                                sep = ",",
                                col.names = "Transmittance"
@@ -63,7 +63,7 @@ spectra <- lapply(spectra, function(x){
   x$Absorbance <- t(
     baseline.modpolyfit(
       t(x$Absorbance),
-      degree = 6
+      degree = 4
     )[[2]]
   )
   return(x)
@@ -89,8 +89,13 @@ spectra_avg <- matrix(nrow = length(spectra[[1]]$Wavenumber))
 
 file_names <- NULL 
 
-for(i in seq(0, length(spectra)-3, 3)){
-  avg <- as.vector((spectra[[i+1]][4] + spectra[[i+2]][4] + spectra[[i+3]][4])/3)
+for(i in seq(0, length(spectra)-5, 5)){
+  avg <- as.vector(
+    (spectra[[i+1]][4] + 
+       spectra[[i+2]][4] + 
+       spectra[[i+3]][4] +
+       spectra[[i+4]][4] +
+       spectra[[i+5]][4])/5)
   spectra_avg <- cbind(spectra_avg, avg)
   file_names <- c(file_names, Compounds[i +1])
   
@@ -99,40 +104,34 @@ spectra_avg <- data.frame(spectra_avg[ , -1])
 colnames(spectra_avg) <- file_names
 ###END###
 
+####Load Isotherm Results####
+isotherm.raw <- lapply(paste0("./Isotherms/", file_names, ".csv"), read.delim)
+
+#Assign Names
+names(isotherm.raw) <- file_names
+
+#Perform Linear Regressions
+isotherm.fit <- lapply(isotherm.raw, function(i) lm(i[[3]] ~ i[[2]],data = i))
+
+#Extract Kd Values from Slopes
+Kd <- data.frame(file_names, row.names = 1, Kd = NA)
+
+for(i in file_names){
+  Kd[i,1] <- round((coef(isotherm.fit[[i]])[2]), 1) 
+}
+####END####
+
 ####Add Compounds + Kd values to Target Variables.csv if necessary####
 #Write the file names of compounds to csv
 write.csv(file_names, file = "File Names.csv")
+
+#Write Kd Values to csv
+write.csv(Kd, file = "Kd.csv")
 
 ## -> Manually copy file names to Target Variables.csv and add in Kd data 
 
 #Load Target Variables Data
 Targets <- read.csv("Target Variables.csv", sep = ",", row.names = 1)
-###END###
-
-####Plot the ATR Spectra####
-wavenumber <- rev(range(spectra[[1]][1]))
-wavenumbers <- as.vector(spectra[[1]][1])
-
-for (i in 1:ncol(spectra_avg)){
-plot(wavenumbers[, 1],
-     spectra_avg[ , i],
-     type = "l",
-     xlim = wavenumber,
-     xlab = "Wavenumber",
-     ylab = "Absorbance",
-     main = names(spectra_avg[i])
-     )
-}
-
-col_set <- rainbow(ncol(spectra_avg))
-matplot(wavenumbers,
-        spectra_avg,
-        type = "l",
-        xlim = wavenumber,
-        col = col_set
-        )
-minor.tick(nx = 5)
-legend("top", legend = names(spectra_avg), lty=c(1,1), lwd=c(2.5,2.5), col = col_set)
 ###END###
 
 
@@ -215,8 +214,7 @@ lapply(names(spectra), function(x){
   minor.tick(nx = 5)
 }
 )
-###End###
-
+####End####
 
 ####Dataframe Preparation for PLS - Full Spectrum####
 spectra.df <- data.frame(row.names = names(spectra_avg))
@@ -225,32 +223,47 @@ spectra.df$Kd <- Targets$Kd
 
 #Log-transform KD Values
 spectra.df$logKd <- log10(Targets$Kd)
-###END###
-
-
-####[DEPRECATED]Make Nested Matrix of MIR Spectra for All Compounds####
-#For Normalized Absorbance
-MIR <- sapply(spectra, function(x){
-  MIR <- as.matrix(x$Normalized) })
-
-MIR <- t(MIR)
-
-colnames(MIR) <- spectra[[1]][ ,1]
-
-spectra.df$MIR <- MIR
-
-rm(MIR)
-
-rownames(spectra.df) <- spectra.df[ ,1]
-
-spectra.df <- spectra.df[ ,-1]
-###END###
-
+####END####
 
 ####Make Nested Matrix of MIR Spectra for All Compounds####
 MIR <- as.matrix(spectra_avg)
 MIR <- t(MIR)
 spectra.df$MIR <- MIR
+####END####
+
+####Plot the ATR Spectra####
+wavenumber <- rev(range(spectra[[1]][1]))
+wavenumbers <- as.vector(spectra[[1]][1])
+
+for (i in 1:ncol(spectra_avg)){
+  plot(wavenumbers[, 1],
+       spectra_avg[ , i],
+       type = "l",
+       xlim = wavenumber,
+       xlab = "Wavenumber",
+       ylab = "Absorbance",
+       main = names(spectra_avg[i])
+  )
+}
+
+col_set <- ifelse(spectra.df[,1] > 500, "red",
+                  ifelse(between(spectra.df[,1], 300, 500), "yellow", "blue"))
+
+matplot(wavenumbers,
+        spectra_avg,
+        type = "l",
+        xlim = wavenumber,
+        col = col_set,
+        xlab = "Wavenumber (1/cm)",
+        ylab = "Absorbance",
+        main = "Overlaid ATR-FTIR Spectra"
+)
+minor.tick(nx = 5)
+legend("top", 
+       legend = c("Kd > 600", "Kd > 300", "Kd < 300"), 
+       fill = c("red", "yellow", "blue")
+       )
+legend("top",legend = names(spectra_avg), fill = col_set)
 ###END###
 
 ####Calculate Second Derivative for Peak Assignment####
@@ -319,23 +332,7 @@ for (i in colnames(spectra_avg)){
   minor.tick(nx = 4)
   abline(v = peaks.list[[i]], col = "red")
 }
-
-###END###
-
-#####[DEPRECATED]For Derivative of Absorbance####
-MIR.deriv <-  sapply(spectra, function(x){
-  MIR.deriv <- as.matrix(x$Deriv) })
-MIR.deriv <- t(MIR.deriv)
-
-colnames(MIR.deriv) <- spectra[[1]][ ,1]
-
-spectra.df$MIR.deriv <- MIR.deriv
-
-rm(MIR.deriv)
-###END###
-
-
-
+####END####
 
 ####Remove CO2 Region & Split Data for Training on Spectra####
 #Make the Wavenumbers Scale
@@ -348,12 +345,14 @@ wavenumbers_NoCO2 <- wavenumbers[c(cm3600_2200, cm1900_650), 1]
 
 spectra.df$MIR <- spectra.df$MIR[, c(cm3600_2200, cm1900_650)]
 set.seed(123)
-split <- sample.split(spectra.df$Kd, SplitRatio = 0.7)
+split <- sample.split(spectra.df$Kd, SplitRatio = 0.8)
 
-train <- spectra.df
+train <- spectra.df[-(45:50),]
 train <- subset(spectra.df, split == T)
+
+test <- spectra.df[45:50,]
 test <- subset(spectra.df, split == F)
-###END###
+####END####
 
 
 ####Run Model on Full Spectrum####
@@ -365,6 +364,9 @@ plsr.fit <- plsr(logKd ~ MIR,
                      center = TRUE,
                      method = "simpls" 
 )
+
+#Change MIR Dimension Names for Easier Plotting
+dimnames(plsr.fit[[3]])[[1]] <- wavenumbers_NoCO2
 
 summary(plsr.fit)
 
@@ -380,6 +382,7 @@ plot(plsr.fit[[2]][,1],
      xlab = "t",
      ylab = "u"
 )
+abline(a = 0, b = 1)
 
 text(x = plsr.fit[[2]][,1], 
      y = plsr.fit[[4]][,1], 
@@ -396,7 +399,7 @@ plot(plsr.fit[[1]],
 #Choose Number of Components
 Num.components <- 2
 
-plot(wavenumbers[c(cm3600_2200, cm1900_650), ],
+plot(wavenumbers_NoCO2,
      plsr.fit[[1]][(1422*Num.components):((1422*Num.components)+1421)],
      type = "l",
      xlim = c(3600,650),
@@ -406,6 +409,61 @@ plot(wavenumbers[c(cm3600_2200, cm1900_650), ],
      )
 abline(h = 0)
 minor.tick(nx = 5)
+
+#Store Coefficients as Object
+Fit.Coefficients <-plsr.fit[[1]][(1422*Num.components):((1422*Num.components)+1421)] 
+
+#Create a List of All Peaks Calculated for Coefficients Below 1800 cm-1
+#peaks.list <- wavenumbers_NoCO2[find_peaks(Fit.Coefficients, m = 3)]
+Fit.deriv <- sgolayfilt(Fit.Coefficients, 
+                        p = 3, 
+                        n = 19, 
+                        m = 2, 
+                        ts = 1
+) 
+
+peaks.list <- wavenumbers_NoCO2[c(find_peaks(Fit.deriv, m = 3),find_peaks(-Fit.deriv, m = 3))]
+
+
+#Retain Only Region Below 1800 cm-1
+peaks.list <- peaks.list[which(peaks.list < 1800)]
+
+###Not quite..
+for (i in 1:length(Fit.deriv)){
+  if(abs(Fit.deriv[i]) > 0.00005) Fit.deriv[i] <- Fit.deriv[i]
+  else Fit.deriv[i] <- NA
+}
+
+#Plot Derivative
+plot(wavenumbers_NoCO2,
+     Fit.deriv,
+     type = "l",
+     xlim = c(1800, 650)
+)
+
+#Plot Coefficients
+plot(wavenumbers_NoCO2,
+     Fit.Coefficients,
+     type = "l",
+     xlim = c(1800, 650)
+     )
+
+abline(v = peaks.list, col = "red")
+
+#plot(wavenumbers_NoCO2,
+#     Fit.Coefficients,
+#     type = "l",
+#     xlim = c(1800,650),
+#     xlab = "Wavenumber",
+#     ylab = "Coefficient",
+#     main = paste(c("Regression Coefficients by Wavenumber", Num.components, "Components"))
+#)
+#abline(v = peaks.list, col = "red")
+
+
+##Working on this: Not functional yet, trying to limit peak find to "real" peaks and troughs (June 21)
+temp <- find_peaks((abs(Fit.Coefficients)>0.005), m = 4)
+abline(v = temp, col = "red")
 ####END####
 
 ####Test to see if coefficients are indeed the regression coefficients Y onto X####
@@ -419,8 +477,8 @@ predict(plsr.fit, ncomp = 1, newdata = test)
 
 #####Plot Fit for Training Data####
 plot(plsr.fit, ncomp = 2, line = TRUE,
-     xlim = c(1, 4),
-     ylim = c(1, 4))#,
+     xlim = c(1.5, 3),
+     ylim = c(1.5, 3))#,
 labels = "numbers")
 #axes = FALSE
 #)
@@ -429,16 +487,17 @@ labels = "numbers")
 
 #Add labels
 text(x = train[,2], 
-     y = plsr.fit[[9]][33:65], 
+     y = plsr.fit[[9]][36:70], 
      labels = paste(gsub("_.*","", names(plsr.fit[[2]][,2])))#, 
      #pos = 3
 )
+RMSEP(plsr.fit)
 ####END####
 
 ####Fit of Test Data####
 plot(plsr.fit, ncomp = 2, line = TRUE, newdata = test,
-     xlim = c(1,4),
-     ylim = c(1,4)
+     xlim = c(1.5,3),
+     ylim = c(1.5,3)
 )
 
 RMSEP(plsr.fit, ncomp = 2, newdata = test)
@@ -449,22 +508,16 @@ predicted_values <- 10^predict(plsr.fit, ncomp = 2, newdata = test)
 
 plot(test[,1],
      predicted_values,
-     type = "p",
-     asp = 1,
-     axes = FALSE
+     type = "p"
      )
 abline( a = 0, b = 1)
-axis(1, pos=0)
-axis(2, pos=0)
 ####END####
 
-
 #Obtain Predicted KD values of Training Data
-predict(plsr.fit, ncomp = 3)
+predict(plsr.fit, ncomp = 2)
 
 #Predict KD values of Test Data
-predict(plsr.fit, ncomp = 3, newdata = test)
-
+predict(plsr.fit, ncomp = 2, newdata = test)
 
 #Plot Loadings of First Three Components
 plot(plsr.fit, 
@@ -477,6 +530,14 @@ plot(plsr.fit,
 abline(h = 0)
 minor.tick(nx = 5)
 ###END###
+
+####Prediction of Wild Sediments####
+plot(plsr.fit, ncomp = 2, line = TRUE, newdata = sediment,
+     xlim = c(1.5,3),
+     ylim = c(1.5,3)
+)
+
+RMSEP(plsr.fit, ncomp = 2, newdata = sediment)
 
 
 ####Build Dataframe of Test Prediction Errors for Later Use####
@@ -495,11 +556,12 @@ RMSEP.table <- data.frame(
 )
 
 colnames(RMSEP.table) <- "Full Spectrum"
+####END####
 
-#Compare homebrew RMSEP to Package RMSEP function on Full Spectrum Model
-#RMSEP.table$Full.Stock <- t(data.frame(
-#  RMSEP(plsr.fit, estimate = "test", newdata = test, intercept = FALSE)$val)
-#)
+####Compare homebrew RMSEP to Package RMSEP function on Full Spectrum Model####
+RMSEP.table$Full.Stock <- t(data.frame(
+  RMSEP(plsr.fit, estimate = "test", newdata = test, intercept = FALSE)$val)
+)
 ####END####
 
 ####Moving Window Partial Least Squares####
@@ -543,13 +605,14 @@ matplot(residues$midpoint,
         xlab = "Wavenumber"
 )
 minor.tick(nx = 5)
+abline(h = 0)
 title(main = "Traces des Residus, Taille de Fenêtre = 5")
 ####END####
 
 ####Define Windows####
 #Window 1
 #Define Spectral Region 1
-window.1 <- which(between(wavenumbers_NoCO2, 2800, 3600) == TRUE)
+window.1 <- which(between(wavenumbers_NoCO2, 1550, 1800) == TRUE)
 
 #Fit PLS
 #Train the Model on Windows of Absorbance Data
@@ -568,7 +631,7 @@ RMSEP(window1.fit)
 
 #Window 2
 #Define Spectral Region 2
-window.2 <- which(between(wavenumbers_NoCO2, 1400, 1850) == TRUE)
+window.2 <- which(between(wavenumbers_NoCO2, 1150, 1450) == TRUE)
 
 #Fit PLS
 #Train the Model on Windows of Absorbance Data
@@ -587,7 +650,7 @@ RMSEP(window2.fit)
 
 #Window 3
 #Define Spectral Region 2
-window.3 <- which(between(wavenumbers_NoCO2, 1100, 1400) == TRUE)
+window.3 <- which(between(wavenumbers_NoCO2, 800, 1100) == TRUE)
 
 #Fit PLS
 #Train the Model on Windows of Absorbance Data
@@ -606,7 +669,7 @@ RMSEP(window3.fit)
 
 #Window 4
 #Define Spectral Region 4
-window.4 <- which(between(wavenumbers_NoCO2, 650, 1100) == TRUE)
+window.4 <- which(between(wavenumbers_NoCO2, 2800, 3000) == TRUE)
 
 #Fit PLS
 #Train the Model on Windows of Absorbance Data
@@ -644,7 +707,7 @@ RMSEC.table <- t(data.frame(RMSEP(plsr.fit, estimate = "CV", intercept = FALSE)$
 
 RMSEC.table <- data.frame(RMSEC.table)
 
-colnames(RMSEC.table)[colnames(RMSEC.table) == "CV"] <- "Full"
+colnames(RMSEC.table)[colnames(RMSEC.table) == "CV"] <- "Full Spectrum"
 
 RMSEC.table$MovingWindow <- t(
   data.frame(RMSEP(combined.fit, estimate = "CV", intercept = FALSE)$val)
@@ -668,12 +731,13 @@ RMSEC.table$Window4 <- t(
 ####END####
 
 ####CSMWPLS on Base Region####
-##Based on RMSEC curve, ideal no. components should be __
+##Based on RMSEC curve of Best Window, ideal no. components should be __
+Num.components <- 2
 
 ##CSMWPLS on Base Region (Window _)
 
 #Select window
-window <- window.4
+window <- window.3
 
 #Clear container
 CSMWPLS <- NULL
@@ -728,7 +792,7 @@ which.min(CSMWPLS[1,])
 
 CSMWPLS[, which.min(CSMWPLS[1,])]
 
-Base.Region <- which(between(wavenumbers_NoCO2, 1039, 1046) == TRUE)
+Base.Region <- which(between(wavenumbers_NoCO2, 1039, 1048) == TRUE)
 
 set.seed(123)
 #Run PLSR on Base Region and Append for General Comparison
@@ -766,7 +830,7 @@ for(w in 4:length(window)){
     base12.fit <- plsr(logKd ~ 
                          MIR[ , Base.Region] +
                          MIR[ , window[j]:window[j + w - 1]],
-                       ncomp = 2, 
+                       ncomp = Num.components, 
                        data = train, 
                        validation = "CV", 
                        scale = FALSE, 
@@ -805,7 +869,7 @@ which.min(CSMWPLS[1,])
 
 CSMWPLS[, which.min(CSMWPLS[1,])]
 
-Base.Region2 <- which(between(wavenumbers_NoCO2, 1429, 1532) == TRUE)
+Base.Region2 <- which(between(wavenumbers_NoCO2, 1218, 1354) == TRUE)
 
 #Run PLSR on Base Region 1 & 2 and Append for General Comparison
 base12.fit <- plsr(logKd ~ MIR[ , Base.Region] + MIR[ , Base.Region2],
@@ -827,7 +891,7 @@ RMSEC.table$Base.Region12 <- t(
 
 ####SCSMWPLS on Base Region12 + Window _####
 #Select window
-window <- window.3
+window <- window.1
 
 #Clear container
 CSMWPLS <- NULL
@@ -846,7 +910,7 @@ for(w in 4:length(window)){
                           MIR[ , Base.Region2] +
                           MIR[ , window[j]:
                                  window[j + w - 1]],
-                        ncomp = 2, 
+                        ncomp = Num.components, 
                         data = train, 
                         validation = "CV", 
                         scale = FALSE, 
@@ -885,7 +949,7 @@ which.min(CSMWPLS[1,])
 
 CSMWPLS[, which.min(CSMWPLS[1,])]
 
-Base.Region3 <- which(between(wavenumbers_NoCO2, 1328, 1357) == TRUE)
+Base.Region3 <- which(between(wavenumbers_NoCO2, 1768, 1775) == TRUE)
 
 #Run PLSR on Base Region 123 and Append for General Comparison
 base123.fit <- plsr(logKd ~ 
@@ -910,7 +974,7 @@ RMSEC.table$Base.Region123 <- t(
 
 ####SCSMWPLS on Base Region123 + Window _####
 #Select window
-window <- window.1
+window <- window.4
 
 #Clear container
 CSMWPLS <- NULL
@@ -930,7 +994,7 @@ for(w in 4:length(window)){
                           MIR[ , Base.Region3] +
                           MIR[ , window[j]:
                                  window[j + w - 1]],
-                        ncomp = 2, 
+                        ncomp = Num.components, 
                         data = train, 
                         validation = "CV", 
                         scale = FALSE, 
@@ -969,7 +1033,7 @@ which.min(CSMWPLS[1,])
 
 CSMWPLS[, which.min(CSMWPLS[1,])]
 
-Base.Region4 <- which(between(wavenumbers_NoCO2, 3593, 3599) == TRUE)
+Base.Region4 <- which(between(wavenumbers_NoCO2, 2993, 2999) == TRUE)
 
 #Run PLSR on Base Region 1234 and Append for General Comparison
 base1234.fit <- plsr(logKd ~ 
@@ -1013,17 +1077,17 @@ RMSEP.table$Base123 <- alex.RMSEP(model = base123.fit, data = test, target = "lo
 
 RMSEP.table$Base1234 <- alex.RMSEP(model = base1234.fit, data = test, target = "logKd")
 
-#### ->Left off here on June 9
 
 #Plot the line of Measured vs. Best Prediction
-plot(base123.fit, ncomp = 1, asp = 1, line = TRUE, newdata = test)
-plot(truncated.fit, ncomp = 1, asp = 1, line = TRUE, newdata = test)
+plot(base1234.fit, ncomp = 2, asp = 1, line = TRUE, newdata = test)
+plot(plsr.fit, ncomp = 2, asp = 1, line = TRUE, newdata = test)
 
-#And second-best fit:
-plot(gas.window2.fit, ncomp = 2, asp = 1, line = TRUE, newdata = test)
+
+#### ->Left off here on June 16
+RMSEP(base1234.fit, ncomp = 2, newdata = test)
 
 #Get Actual Prediction Values
-predict(base123.fit, ncomp = 1, newdata = test)
+predict(base1234.fit, ncomp = 2, newdata = test)
 
 #Plot Loadings of First Two Components
 dimnames(base123.fit[[3]])[[1]] <- gsub(".*]", "", dimnames(base123.fit[[3]])[[1]])
